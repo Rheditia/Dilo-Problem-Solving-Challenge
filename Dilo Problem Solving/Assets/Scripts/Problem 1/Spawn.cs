6e6bf45e4f14462120e62f47a7a419111683a7c6
Rheditia <rheditiaf2406@gmail.com>
2021-12-12 23:12:27 +0700

    Finished Problem 1

---
+++ Dilo Problem Solving/Assets/Scripts/Problem 1/Spawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] GameObject playerPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(playerPrefab, transform.position, transform.rotation);
+    }
+}
